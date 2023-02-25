6d1be8a06409102591e5aa25a03c4c547e8f4b36
accazeta <f.panzeri24@studenti.unibg.it>
2023-02-25 22:42:05 +0100

    replicated implementation for file scanning also for folders

--- SIV.py
+++ SIV.py
@@ -29,14 +29,34 @@ def scan_folder(root_folder, csv_writer):
         path = os.path.join(root_folder, filename)
         if os.path.isdir(path):
             num_dirs += 1
-            
+            #--------------writing phase for directories-----------------
+            # Save only the name of the directory
+            name = os.path.basename(os.path.dirname(path))
+            size = None # The instructions say that only the size of files should be saved. This is for dirs.
+            # get the owner name
+            dir_owner_uid = os.stat(path).st_uid
+            dir_owner_name = pwd.getpwuid(dir_owner_uid).pw_name
+            # get the group name
+            dir_group_uid = os.stat(path).st_gid
+            dir_group_name = grp.getgrgid(dir_group_uid).gr_name
+            # get the directory permissions
+            permissions = oct(os.stat(path).st_mode & 0o777)
+            # Assumption: the last modification datetime of a folder can be ambiguous (some might say it's the
+            # same of the last modified file, some might say it doesn't make sense). I decided to follow the latter.
+            date_time = None
+            # hash: None
+            hashed_value = None
+            # save all the values in a list before writing to the csv file
+            toBeWritten = [name, size, dir_owner_name, dir_group_name, permissions, date_time, hashed_value, root_folder]
+            # writes to the csv
+            csv_writer.writerow(toBeWritten)
             # next folder
             sub_files, sub_dirs = scan_folder(path, csv_writer)
             num_files += sub_files
             num_dirs += sub_dirs
         else:
             num_files += 1
-            #--------------writing phase-----------------
+            #--------------writing phase for files-----------------
             # get the size
             size = os.path.getsize(path)
             # get the owner name
@@ -57,7 +77,7 @@ def scan_folder(root_folder, csv_writer):
             elif args.hash_function == "sha1":
                 computed_message_digest = calculate_hash(path, hashlib.sha1())
             # save all the values in a list before writing to the csv file
-            toBeWritten = [filename, size, file_owner_name, file_group_name, permissions, formatted_datetime, computed_message_digest, root_folder]
+            toBeWritten = [filename, size, file_owner_name, file_group_name, permissions, formatted_datetime, computed_message_digest, path]
             # writes to the csv
             csv_writer.writerow(toBeWritten)
 
@@ -123,7 +143,9 @@ if __name__ == "__main__":
                     else:
                         with open(verFilePath + ".csv", "w", newline="") as csv_file:
                             writer = csv.writer(csv_file)
-                            scan_folder(dirPath, writer)
+                            writer.writerow(['Name', 'Size (Kb)', 'Owner', 'Group', 'Permission levels', 'Last modification date time', 'Hash', 'Path'])
+                            num_files, num_dirs = scan_folder(dirPath, writer)
+                            print(f"In total {num_files} files and {num_dirs} directories have been scanned!")
             
         except Exception as e:
             print(str(e) + "\n")
